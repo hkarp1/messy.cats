f1de37f9fcd903e63fadde65510fd5230bdaf034
Andrew Hennessy <andrewhennessy@Andrews-MacBook-Pro-3.local>
2021-06-13 16:43:20 -0400

    @hkarp1 its "messy" not "messey"

--- R/method_check.R
+++ R/method_check.R
@@ -22,3 +22,10 @@
 # # jw<-cat_match(dmu$DEER_MGM_1,deerpop$DMU,method="jw",p=0.1,return_dists = T)
 # # lcs<-cat_match(dmu$DEER_MGM_1,deerpop$DMU,method="lcs",p=0.1,return_dists = T)
 # # cos<-cat_match(dmu$DEER_MGM_1,deerpop$DMU,method="cosine",p=0.1,return_dists = T)
+
+
+colnames(messy_caterpillars)=c("Caterpillar Species","Avg Weight (g)")
+clean_caterpillars
+
+
+
