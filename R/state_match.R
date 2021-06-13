184f77d75c39fe01a980d5aaa4fdee5244a493a2
Andrew Hennessy <andrewhennessy@Andrews-MacBook-Pro-3.local>
2021-06-12 21:25:06 -0400

    taking out my dumbshit examples

--- R/state_match.R
+++ R/state_match.R
@@ -14,18 +14,18 @@ cat_match(messy_states,state.name,method = "jw",return_dists = return_dists,retu
 }
 
 
-#### Examples ####
-messy_states1 <- readRDS("C:/Users/abhen/Desktop/messy.cats/data/messy_states1.rds")
-messy_states2 <- readRDS("C:/Users/abhen/Desktop/messy.cats/data/messy_states2.rds")
-
-# normal match with not very messy states list
-state_match(messy_states1,return_dists=T,p=0.05) -> df
-
-# normal match with messier states
-state_match(messy_states2,return_dists=T,p=0.05) -> df2
-
-# match returning lists with messier states
-state_match(messy_states2,return_dists=T,p=0.05,return_lists = 3) -> df3
-
-# match by choosing with messier states
-state_match(messy_states2,return_dists=T,threshold=0.2,p=0.05,return_lists = 5,pick_lists=T) -> df4
+# #### Examples ####
+# messy_states1 <- readRDS("C:/Users/abhen/Desktop/messy.cats/data/messy_states1.rds")
+# messy_states2 <- readRDS("C:/Users/abhen/Desktop/messy.cats/data/messy_states2.rds")
+#
+# # normal match with not very messy states list
+# state_match(messy_states1,return_dists=T,p=0.05) -> df
+#
+# # normal match with messier states
+# state_match(messy_states2,return_dists=T,p=0.05) -> df2
+#
+# # match returning lists with messier states
+# state_match(messy_states2,return_dists=T,p=0.05,return_lists = 3) -> df3
+#
+# # match by choosing with messier states
+# state_match(messy_states2,return_dists=T,threshold=0.2,p=0.05,return_lists = 5,pick_lists=T) -> df4
