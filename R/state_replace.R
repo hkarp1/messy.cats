720b88ecd1066e8b873aff05c41f55cfd899e161
Abhennessy99 <ahennessy@wesleyan.edu>
2021-06-20 03:38:27 -0400

    some documentation copy-pasting

--- R/state_replace.R
+++ R/state_replace.R
@@ -1,20 +1,29 @@
-
 #' @title state_replace
-#' @description Wrapper of cat_replace() Calls cat_replace on input with clean vector state.name, a vector of the names of all U.S. states
-#' @param messy_states PARAM_DESCRIPTION
-#' @param threshold PARAM_DESCRIPTION, Default: NA
-#' @param method PARAM_DESCRIPTION, Default: 'jw'
-#' @param q PARAM_DESCRIPTION, Default: 1
-#' @param p PARAM_DESCRIPTION, Default: 0
-#' @param bt PARAM_DESCRIPTION, Default: 0
-#' @param useBytes PARAM_DESCRIPTION, Default: FALSE
-#' @param weight PARAM_DESCRIPTION, Default: c(d = 1, i = 1, t = 1)
+#' @description Wrapper of cat_replace() using inputted messy vector and a the
+#' built in clean vector state.name, a vector of the names of all U.S. states.
+#' @param messy_states Vector containing the messy state names that will be replaced
+#' by the closest match from state.name
+#' @param threshold The maximum distance that will form a match. If this argument
+#' is specified, any element in the messy vector that has no match closer than
+#' the threshold distance will be replaced with NA. Default: NA
+#' @param method The type of string distance calculation to use. Possible methods
+#' are : osa, lv, dl, hamming, lcs, qgram, cosine, jaccard, jw, and soundex.
+#' See package stringdist for more information. Default: 'jw'
+#' @param q Size of the q-gram used in string distance calculation. Default: 1
+#' @param p Only used with method "jw", the Jaro-Winkler penatly size. Default: 0
+#' @param bt Only used with method "jw" with p > 0, Winkler's boost threshold. Default: 0
+#' @param useBytes Whether or not to perform byte-wise comparison. Default: FALSE
+#' @param weight Only used with methods "osa" or "dl", a vector representing the
+#' penalty for deletion, insertion, substitution, and transposition,
+#' in that order. Default: c(d = 1, i = 1, t = 1)
 #' @return OUTPUT_DESCRIPTION
 #' @details DETAILS
 #' @examples
 #' \dontrun{
 #' if(interactive()){
 #'  #EXAMPLE1
+#'  lst <- c("Indianaa", "Wisvconsin", "aLaska", "NewJersey", "Claifoarni")
+#'  fixed <- state_replace(lst)
 #'  }
 #' }
 #' @rdname state_replace
@@ -26,6 +35,3 @@ state_replace <- function(messy_states, threshold = NA,
   cat_replace(messy_states, state.name, threshold, method = "jw", q, p, bt, useBytes, weight)
 }
 
-#Example
-lst <- c("Indianaa", "Wisvconsin", "aLaska", "NewJersey", "Claifoarni")
-fixed <- state_replace(lst)
