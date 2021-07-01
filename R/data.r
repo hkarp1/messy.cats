4da69ed144dc13bb32eeede40ff92d1dc751753d
Andrew Hennessy <ahennessy@wesleyan.edu>
2021-07-01 12:50:49 -0400

    did some doc

--- R/data.r
+++ R/data.r
@@ -11,21 +11,21 @@
 #' @description Dataframe with caterpillar counts from three summers.
 #' @format A data frame with 74 rows and 3 variables:
 #' \describe{
-#'   \item{\code{species}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{count}}{integer COLUMN_DESCRIPTION}
-#'   \item{\code{year}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{species}}{character Full latin names of 29 caterpillar species.}
+#'   \item{\code{count}}{integer Randomly generated fake counts of the caterpillars.}
+#'   \item{\code{year}}{double Year of caterpillar observations.}
 #'}
 #' @details DETAILS
 "clean_caterpillars"
-
 
 #' @title messy_caterpillars
 #' @description DATASET_DESCRIPTION
 #' @format A data frame with 39 rows and 3 variables:
 #' \describe{
-#'   \item{\code{CaterpillarSpecies}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{Avg Weight (mg)}}{double COLUMN_DESCRIPTION}
-#'   \item{\code{Avg Length (cm)}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{CaterpillarSpecies}}{character Full latin names of 39 caterpillar
+#'   species with spelling and formatting errors.}
+#'   \item{\code{Avg Weight (mg)}}{double Randomly generated fake weight data for each caterpillar species.}
+#'   \item{\code{Avg Length (cm)}}{double Randomly generated fake length data for each caterpillar species.}
 #'}
 #' @details DETAILS
 "messy_caterpillars"
@@ -34,7 +34,7 @@
 #' @description US State names with 1 character randomly changed.
 #' @format A data frame with 50 rows and 1 variables:
 #' \describe{
-#'   \item{\code{messy_states1}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{messy_states1}}{character All 50 US states with 1 randomly changed character.}
 #'}
 #' @details DETAILS
 "messy_states1"
@@ -43,7 +43,7 @@
 #' @description US State names with 2 characters randomly changed.
 #' @format A data frame with 50 rows and 1 variables:
 #' \describe{
-#'   \item{\code{messy_states2}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{messy_states2}}{character All 50 US states with 2 randomly changed characters.}
 #'}
 #' @details DETAILS
 "messy_states2"
