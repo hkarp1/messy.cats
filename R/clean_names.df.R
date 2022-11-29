880ee58931beb89cc17741496e5a56866989c6b6
hkarp1 <hkarp@wesleyan.edu>
2022-11-28 22:16:47 -0500

    new doc

---
+++ R/clean_names.df.R
@@ -0,0 +1,9 @@
+#' @title clean_names.df
+#' @description Data set of clean names
+#' @format A data frame with 20 rows and 2 variables:
+#' \describe{
+#'   \item{\code{first}}{character Clean first names}
+#'   \item{\code{last}}{character Clean last names}
+#'}
+#' @details An example data that can be used in testing messy.cats functions
+"clean_names.df"
