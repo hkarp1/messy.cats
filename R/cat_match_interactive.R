e44227d9e4f6b751a674405aba36adbadde5330c
hkarp1 <hkarp@wesleyan.edu>
2022-12-02 17:18:00 -0500

    cat_match_interactive

--- R/cat_match_interactive.R
+++ R/cat_match_interactive.R
@@ -1,5 +1,5 @@
-#' @title cat_match
-#' @description `cat_match()` matches the contents of a messy vector with
+#' @title cat_match_interactive
+#' @description `cat_match_interactive()` matches the contents of a messy vector with
 #' the closest match in a clean vector. The closest match can be found
 #' using a variety of different string distance measurement options.
 #' @param messy_v The messy string vector that will be restructured. This can come in the form
@@ -8,6 +8,8 @@
 #' Again, this argument can be a dataframe column or vector.
 #' @param return_dists If set to TRUE the distance between the matched strings will
 #' be returned as a third column in the output dataframe, Default: TRUE
+#' @param return_lists Return list of top X matches, Default: NA
+#' @param pick_lists Set to TRUE to manually choose matches, Default: FALSE
 #' @param threshold The maximum distance that will form a match. If this argument
 #' is specified, any element in the messy vector that has no match closer than
 #' the threshold distance will be replaced with NA, Default: NA
@@ -36,20 +38,31 @@
 #' method works particularly well for messy user-inputted data that often suffers
 #' from transposition or misspelling errors.
 #'
+#' `cat_match_interactive()` is meant as an exploratory tool to discover how the elements
+#' of two vectors will match using string distance measures, and has added functionality
+#' to solve issues by hand and create a dataframe that can be used to create custom
+#' matches between the clean and messy vectors.
 #' @examples
 #' if(interactive()){
 #'  messy_trees = c("red oak", "williw", "hemluck", "white elm",
 #'  "fir tree", "birch tree", "pone", "dagwood", "mople")
 #'  clean_trees = c("oak", "willow", "hemlock", "elm", "fir", "birch", "pine", "dogwood", "maple")
-#'  matched_trees = cat_match(messy_trees, clean_trees)
+#'  matched_trees = cat_match_interactive(messy_trees, clean_trees)
 #'  }
-#' @rdname cat_match
-#' @import dplyr
+#' @seealso
+#'  \code{\link[varhandle]{unfactor}}
+#'  \code{\link[rapportools]{is.boolean}}, \code{\link[rapportools]{min}}
+#'  \code{\link[stringdist]{stringdist}}
+#'  \code{\link[dplyr]{arrange}}, \code{\link[dplyr]{slice}}
+#' @rdname cat_match_interactive
 #' @export
+#' @importFrom varhandle unfactor
+#' @importFrom rapportools is.boolean min
+#' @importFrom stringdist stringdistmatrix
+#' @importFrom dplyr arrange slice
+#'
 
-
-
-cat_match <- function(messy_v, clean_v, return_dists = TRUE,
+cat_match_interactive <- function(messy_v, clean_v, return_dists = TRUE, return_lists = NA, pick_lists = FALSE,
                       threshold = NA, method = "jw", q = 1, p = 0, bt = 0,
                       useBytes = FALSE, weight=c(d=1, i=1, t=1)) {
 
@@ -75,12 +88,16 @@ cat_match <- function(messy_v, clean_v, return_dists = TRUE,
     stop("Argument bt must be a number")
   } else if (!rapportools::is.boolean(return_dists)) {
     stop("Argument unique must be a boolean")
+  } else if (!rapportools::is.boolean(pick_lists)) {
+    stop("Argument unique must be a boolean")
   } else if (!rapportools::is.boolean(useBytes)) {
     stop("Argument unique must be a boolean")
   } else if (!(method %in% c("osa", "lv", "dl", "hamming", "lcs", "qgram",
                              "cosine", "jaccard", "jw","soundex"))) {
     stop("Please use only a method available to the stringdist function in the
          stringdist package")
+  } else if (!is.na(return_lists) & !is.numeric(return_lists)) {
+    stop("Argument return_lists must be either NA or numeric")
   } else if (!is.na(threshold) & !is.numeric(threshold)) {
     stop("Argument threshold must be either NA or numeric")
   } else if (!is.vector(weight) | length(weight) != 3) {
@@ -102,33 +119,100 @@ cat_match <- function(messy_v, clean_v, return_dists = TRUE,
   new_var <- c()
 
   if (!is.na(threshold)) {
-    for (i in 1:ncol(x)) {
-      min <- rapportools::min(x[[i]])
-      min.plc <- which.min(x[[i]])
-      if (min <= threshold) {
-        new_var <- new_var %>% append(u_clean_v[min.plc])
-      } else {
-        new_var <- new_var %>% append(NA)
+    if (!is.na(return_lists)) {
+      acc_dists = c()
+      new_var = c()
+
+
+      for (i in 1:ncol(x)) {
+        x[i] %>% dplyr::arrange(x[i]) %>% dplyr::slice(1:return_lists) -> t
+        t$matches = row.names(t)
+        colnames(t)[1] = "dists"
+        if (rapportools::min(unlist(t$dists)) <= threshold) {
+          t %>% dplyr::slice(1) -> t
+          new_var[[i]] = unlist(t$matches)
+        } else {
+          new_var[[i]] = unlist(t$matches)
+        }
+
+        if (return_dists == TRUE) {
+          acc_dists[[i]] = round(unlist(t$dists), 4)
+        }
+      }
+
+    } else {
+      for (i in 1:ncol(x)) {
+        min <- rapportools::min(x[[i]])
+        min.plc <- which.min(x[[i]])
+        if (min <= threshold) {
+          new_var <- new_var %>% append(u_clean_v[min.plc])
+        } else {
+          new_var <- new_var %>% append(NA)
+        }
       }
     }
 
   } else {
+    if (!is.na(return_lists)) {
+      acc_dists = c()
+      new_var = c()
 
-    for (i in 1:ncol(x)) {
-      min <- which.min(x[[i]])
-      new_var <- new_var %>% append(u_clean_v[min])
+      for (i in 1:ncol(x)) {
+        x[i] %>% dplyr::arrange(x[i]) %>% dplyr::slice(1:return_lists) -> t
+        t$matches = row.names(t)
+        colnames(t)[1] = "dists"
+        new_var[[i]] = unlist(t$matches)
+        if (return_dists == TRUE) {
+          acc_dists[[i]] = round(unlist(t$dists), 4)
+        }
+      }
+
+    } else {
+      for (i in 1:ncol(x)) {
+        min <- which.min(x[[i]])
+        new_var <- new_var %>% append(u_clean_v[min])
+      }
     }
-
   }
 
   df = data.frame(y = u_messy_v)
   df$x = new_var
 
-  if (return_dists == TRUE) {
+  if (return_dists == TRUE & is.na(return_lists)) {
     df$dists = round(sapply(x, extract_min), 4)
+  } else if (return_dists == TRUE & !is.na(return_lists)) {
+    df$dists = acc_dists
   }
 
   colnames(df)[1:2] = c("bad", "match")
+
+  if (!is.na(return_lists) & pick_lists == TRUE) {
+    changing = df[lapply(df$match, length) > 1, ]
+    df = df[!lapply(df$match, length) > 1, ]
+
+    for (i in 1:nrow(changing)) {
+      bad = changing[i, "bad"]
+      matches = unlist(changing[i, "match"])
+      dists = unlist(changing[i, "dists"])
+      for (p in 1:return_lists) {
+        print(paste0(matches[[p]], " has a distance of ", dists[[p]], " from ", bad))
+        print(paste0("Enter ", p, " to keep this match"))
+      }
+      print("Enter 0 to force a non-match (make the string match to NA)")
+      which = as.numeric(readline(prompt = "Which one would you like to keep?"))
+      if (which == 0) {
+        changing[i, "match"] = NA
+        changing[i, "dists"] = NA
+      } else {
+        changing[i, "match"] = changing[i, "match"][[1]][which]
+        changing[i, "dists"] = changing[i, "dists"][[1]][which]
+      }
+    }
+
+    df = rbind(df, changing)
+    df$match = unlist(df$match)
+    df$dists = unlist(df$dists)
+  }
 
   return(df)
 
--- R/testing.R
+++ R/testing.R
@@ -1,56 +1,56 @@
-# fix_typos ----
-rep(clean_caterpillars$species,clean_caterpillars$count) -> clean_caterpillars_rep
-
-append(clean_caterpillars_rep,messy_caterpillars$CaterpillarSpecies) -> typo_caterpillars
-
-typo_df <- as.data.frame(typo_caterpillars)
-
-typo_v = typo_caterpillars
-occ_ratio = 10
-thr = 0.3
-
-fix_typos(typo_v = typo_v, thr = 0.3, occ_ratio = 10) %>% unique
-
-counties <- c(
-  "Litchfield",
-  "Hartford",
-  "Tolland",
-  "Windham",
-  "Fairfield",
-  "New Haven",
-  "Middlesex",
-  "New London"
-)
-
-counties_typo <- c(
-  "Litchfeld",
-  "Hartferd",
-  "Tolland",
-  "Wind Ham",
-  "Fairfield",
-  "New Heven",
-  "Midlsex",
-  "NwLndn"
-)
-
-counties_v <- append(rep(counties,10),counties_typo)
-
-counties_v -> typo_v
-thr = 0.5
-occ_ratio = 5
-
-fix_typos(typo_v = counties_v, thr = 0.5, occ_ratio = 5) %>% unique
-
-# this is in the data folder
-readr::read_csv("data/aus_cities_typos.csv") -> aus_df
-aus_typos = aus_df$City
-thr = 0.5
-occ_ratio = 10
-
-fix_typos(aus_typos, thr = 0.5, occ_ratio = 10) %>% unique
-
-# i feel like this has potential but there are a ton of weird "corrections" that
-# are super not intuitive
-read_csv("github_typos.csv") -> gh_df
-gh_df %>% select(wrong, correct) -> gh_df2
-gh_typos <- append(gh_df2$wrong,gh_df2$correct)
+# # fix_typos ----
+# rep(clean_caterpillars$species,clean_caterpillars$count) -> clean_caterpillars_rep
+#
+# append(clean_caterpillars_rep,messy_caterpillars$CaterpillarSpecies) -> typo_caterpillars
+#
+# typo_df <- as.data.frame(typo_caterpillars)
+#
+# typo_v = typo_caterpillars
+# occ_ratio = 10
+# thr = 0.3
+#
+# fix_typos(typo_v = typo_v, thr = 0.3, occ_ratio = 10) %>% unique
+#
+# counties <- c(
+#   "Litchfield",
+#   "Hartford",
+#   "Tolland",
+#   "Windham",
+#   "Fairfield",
+#   "New Haven",
+#   "Middlesex",
+#   "New London"
+# )
+#
+# counties_typo <- c(
+#   "Litchfeld",
+#   "Hartferd",
+#   "Tolland",
+#   "Wind Ham",
+#   "Fairfield",
+#   "New Heven",
+#   "Midlsex",
+#   "NwLndn"
+# )
+#
+# counties_v <- append(rep(counties,10),counties_typo)
+#
+# counties_v -> typo_v
+# thr = 0.5
+# occ_ratio = 5
+#
+# fix_typos(typo_v = counties_v, thr = 0.5, occ_ratio = 5) %>% unique
+#
+# # this is in the data folder
+# readr::read_csv("data/aus_cities_typos.csv") -> aus_df
+# aus_typos = aus_df$City
+# thr = 0.5
+# occ_ratio = 10
+#
+# fix_typos(aus_typos, thr = 0.5, occ_ratio = 10) %>% unique
+#
+# # i feel like this has potential but there are a ton of weird "corrections" that
+# # are super not intuitive
+# read_csv("github_typos.csv") -> gh_df
+# gh_df %>% select(wrong, correct) -> gh_df2
+# gh_typos <- append(gh_df2$wrong,gh_df2$correct)
